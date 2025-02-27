da283f4ac5fd2d7464a9547dea524460641fbd42
lminjiii <lminjiiiii@gmail.com>
2025-02-27 17:55:49 +0900

    #2. 게시글 및 댓글 조회 완료

---
+++ src/main/java/com/my/articles/entity/Comment.java
@@ -0,0 +1,24 @@
+package com.my.articles.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+
+@Entity
+@Getter
+@Setter
+public class Comment {
+    // 댓글의 개별 primary key
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String nickname;
+    @Column(nullable = false, length = 1000)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "article_id")
+    private Article article;
+}
